08dbc8f436e67afc68afedf411f6fdf43d75dac4
Celia Rangel <cars_260@hotmail.com>
2016-04-07 18:28:30 -0500

    Caso Contaminación

---
+++ corr.R
@@ -0,0 +1,18 @@
+corr <- function(directorio, horizonte = 0) {
+    source("completos.R")
+    gu <- completos(directorio)
+    ids <- gu[gu["nobs"] > horizonte, ]$id
+    corr2 <- numeric()
+    
+    for(i in ids) {
+        dirFinal <- read.csv(paste(directorio, "/", formatC(i, width = 3, flag = "0"), 
+                                 ".csv", sep = "")) #ejemplo: specdata/001.csv
+        ce<- dirFinal[complete.cases(dirFinal), ] #casos completos del dirFinal
+        corr2 <- c(corr2, cor(ce$sulfate, ce$nitrate)) #Vector de correlacion
+    }
+    return(corr2) #regresa el vector de correlacion
+}
+cr <- corr("specdata", 1)
+cr
+head(corr("specdata",400))
+
---
+++ mediacontaminante.R
@@ -0,0 +1,16 @@
+mediacontaminante <- function(directorio, contaminante, id = 1:332) {
+    data <-numeric()
+    
+    for(i in id) {
+        dirFinal<- read.csv(paste(directorio, "/", formatC(i, width = 3, flag = "0"), 
+                                ".csv", sep = "")) #ejemplo: specdata/001.csv
+        data <-c(data, dirFinal[[contaminante]]) #vector de tamaño uno que indica
+        #el nombre del contaminante
+    }
+    return(mean(data, na.rm = TRUE)) #regresa la media del contaminante ignorando Na
+}
+
+mediacontaminante("specdata", "nitrate", 70:72)    
+mediacontaminante("specdata", "sulfate", 1:10)    
+mediacontaminante("specdata", "nitrate", 23)
+
